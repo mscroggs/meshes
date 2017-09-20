075db343177f576aaa159f885ea57816ce7a5c30
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2017-09-20 11:03:06 +0100

    Algorithm 1

--- core.py
+++ core.py
@@ -5,9 +5,10 @@ except ImportError:
         raise ImportError("Could not import Bempp.")
 
 class Mesh(object):
-    def __init__(self, h=0.1, **kwargs):
+    def __init__(self, h=0.1, mesh_algorithm=1, **kwargs):
         self.kwargs = kwargs
         self.h = h
+        self.mesh_algorithm = mesh_algorithm
 
     def as_bempp_grid(self):
         return to_bempp(self.as_geo_string())
@@ -15,7 +16,7 @@ class Mesh(object):
     def as_geo_string(self):
         geo =  "lc = " + str(self.h) + ";"
         geo += self.geo()
-        geo += "\nMesh.Algorithm = 6;"
+        geo += "\nMesh.Algorithm = " + str(self.mesh_algorithm) + ";"
         return geo
 
     def geo(self):
